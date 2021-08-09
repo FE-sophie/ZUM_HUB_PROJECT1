7bd4557051be267a4843be07491a99c9c119b4ed
FE-sophie <saeyoung19@gmail.com>
2021-08-10 03:42:14 +0900

    feat:  데이터 응답시  로컬스토리지에 데이터 저장 및 API 호출 버그 수정

--- FrontEnd/src/modules/api/dataApi.js
+++ FrontEnd/src/modules/api/dataApi.js
@@ -1,5 +1,5 @@
-import appStore, { GET_LOADING, NOT_FOUND } from '../../store/appStore.js';
-const { dispatch } = appStore;
+import appStore, { NOT_FOUND } from '../../store/appStore.js';
+const { dispatch, getState } = appStore;
 
 const request = {
   get(url) {
@@ -13,58 +13,57 @@ const request = {
     });
   },
 };
-export const getMainDataApi = async ({ main }) => {
-  const mainList = Object.keys(main);
+
+//메인페이지 API호출 함수
+export const getMainDataApi = async () => {
+  const mainList = ['life', 'food', 'trip', 'culture'];
   let dataList = {};
   try {
-    dispatch({ type: GET_LOADING, payload: true });
     mainList.forEach(async sectionName => {
       dataList[sectionName] = await getDataApi(sectionName, 'main');
-      dispatch({ type: GET_LOADING, payload: false });
     });
+    localStorage.setItem('main', JSON.stringify(dataList));
     return dataList;
   } catch (error) {
     console.log(`Error:${error}`);
-    // dispatch({ type: NOT_FOUND });
+    dispatch({ type: NOT_FOUND });
   }
 };
 
+//서브페이지 API호출 함수
 export const getDataApi = async (pathName, pageType) => {
   let data = [];
   try {
-    dispatch({ type: GET_LOADING, payload: true });
     const res = await request.get(`http://localhost:3000/api/content/${pathName}`);
     data = await res.json();
-    dispatch({ type: GET_LOADING, payload: false });
+    pageType === 'sub' && localStorage.setItem(pathName, JSON.stringify(data));
     data = pageType === 'sub' ? data.splice(0, 12) : data.splice(0, 3);
     return data;
   } catch (error) {
     console.log(`Error: ${error}`);
-    // dispatch({ type: NOT_FOUND });
   }
 };
 
+//랭킹 데이터 요청 API 호출 함수
 export const getBestDataApi = async () => {
   let best = [];
   try {
-    dispatch({ type: GET_LOADING, payload: true });
     const res = await request.get('http://localhost:3000/api/best');
     best = await res.json();
-    dispatch({ type: GET_LOADING, payload: false });
+    localStorage.setItem('best', JSON.stringify(best));
     best = best.splice(0, 12);
-    return await best;
+    return best;
   } catch (error) {
     console.log(error);
-    // dispatch({ type: NOT_FOUND });
+    dispatch({ type: NOT_FOUND });
   }
 };
 export const postBookMarkApi = async ([route, id]) => {
   let bookmark = [];
   try {
-    dispatch({ type: GET_LOADING, payload: true });
     const res = await request.post('http://localhost:3000/api/bookmark', { route, id });
     bookmark = await res.json();
-    dispatch({ type: GET_LOADING, payload: false });
+    localStorage.setItem('bookmark', JSON.stringify(bookmark));
     return bookmark;
   } catch (error) {
     console.log(error);
@@ -74,10 +73,8 @@ export const postBookMarkApi = async ([route, id]) => {
 export const getDetailApi = async url => {
   let detail = {};
   try {
-    // dispatch({ type: GET_LOADING, payload: true });
     const res = await request.get(url);
     detail = await res.json();
-    // dispatch({ type: GET_LOADING, payload: false });
   } catch (error) {
     console.log(error);
     dispatch({ type: NOT_FOUND });
