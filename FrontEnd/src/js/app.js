2f64b94db6eab298abbdc322d5f67381067601ed
FE-sophie <saeyoung19@gmail.com>
2021-08-09 21:28:56 +0900

    fix: 주소창에 url 입력될때마다 렌더링 안되는 버그 수정

--- FrontEnd/src/js/app.js
+++ FrontEnd/src/js/app.js
@@ -24,73 +24,65 @@ import Loading from '../components/Loading.js';
 const { dispatch, subscribe, getState } = appStore;
 
 const appRender = (type, path) => {
+  //상태 불러오기
   let state = getState();
+
+  //app 요소 찾기
   const $app = document.querySelector('#app');
+
+  //app에 header 추가
+  $app.innerHTML = Header(state, path);
+
+  const $header = document.querySelector('.header');
+
+  const $main = document.querySelector('.main');
+  const $sub = document.querySelector('.sub');
+  const $best = document.querySelector('.best');
+  const $loading = document.querySelector('.loading');
+  const $error = document.querySelector('.error');
+
+  $main && $main.remove();
+  $sub && $sub.remove();
+  $best && $best.remove();
+  $loading && $loading.remove();
+  $error && $Error.remove();
 
   if (type === 'home') {
     //초기 홈화면
-    $app.innerHTML = Header(state) + Main(state) + Best(state);
-
-    //헤더 네비게이션 이벤트 핸들링
-    const $appNavBar = document.querySelector('.navBar');
-    $appNavBar.addEventListener('click', ({ target }) => {
-      if (!target.matches('li')) return;
-      const pathName = target.getAttribute('route');
-
-      historyRouterPush(pathName);
-
-      //네비게시연 클릭시 active클래스 추가
-      [...$appNavBar.children].forEach(el => el.classList.remove('active'));
-      target.classList.add('active');
-    });
-
-    //로고 클릭시 이벤트
-    const $logo = document.querySelector('.logo');
-
-    $logo.addEventListener('click', e => {
-      [...$appNavBar.children].forEach((el, i) => {
-        if (!i) {
-          el.classList.add('active');
-        } else {
-          el.classList.remove('active');
-        }
-        historyRouterPush('/');
-      });
-    });
+    $app.insertAdjacentHTML('beforeend', Main(state) + Best(state));
+  }
+  //페이지에 따른 렌더링 구현
+  else if (type === 'loading') {
+    $header.insertAdjacentHTML('afterend', Loading(state));
+  } else if (type === 'error') {
+    $header.insertAdjacentHTML('afterend', Error(state));
+  } else if (type === 'sub') {
+    $header.insertAdjacentHTML('afterend', SubPage(state, path));
   } else {
-    //노트 찾기
-    const $header = document.querySelector('.header');
-    const $main = document.querySelector('.main');
-    const $sub = document.querySelector('.sub');
-    const $best = document.querySelector('.best');
-    const $loading = document.querySelector('.loading');
-    const $error = document.querySelector('.error');
-
-    //새 렌더링시 기존 html 요소 제거
-    $main && $main.remove();
-    $sub && $sub.remove();
-    $best && $best.remove();
-    $loading && $loading.remove();
-    $error && $Error.remove();
-
-    //페이지에 따른 렌더링 구현
-    // if (type === 'loading') {
-    //   $app.innerHTML = Loading(state);
-    // } else if (type === 'error') {
-    //   $header.insertAdjacentHTML('afterend', Error(state));
-    // } else
-    if (type === 'sub') {
-      $header.insertAdjacentHTML('afterend', SubPage(state, path));
-    } else if (type === 'main') {
-      $header.insertAdjacentHTML(
-        'afterend',
-        state.loading ? Loading(state) : Main(state) + Best(state),
-      );
-    } else {
-      // $header.insertAdjacentHTML('afterend', Detail(state));
-    }
+    $header.insertAdjacentHTML('afterend', Detail(state));
   }
 
+  //네비게이션 라우트 핸들링 이벤트핸들러
+  const $appNavBar = document.querySelector('.navBar');
+  $appNavBar.addEventListener('click', ({ target }) => {
+    if (!target.matches('li')) return;
+    const pathName = target.getAttribute('route');
+    historyRouterPush(pathName);
+  });
+
+  //로고 클릭시 이벤트
+  const $logo = document.querySelector('.logo');
+
+  $logo.addEventListener('click', e => {
+    [...$appNavBar.children].forEach((el, i) => {
+      if (!i) {
+        el.classList.add('active');
+      } else {
+        el.classList.remove('active');
+      }
+      historyRouterPush('/');
+    });
+  });
   //북마크 이벤트 핸들러 등록
   const $bookmark = document.querySelectorAll('.bookmark');
   $bookmark.forEach(el =>
@@ -106,41 +98,50 @@ const appRender = (type, path) => {
     }),
   );
 };
+// subscribe(GET_APP_VIEW, () => appRender('home', hash));
+
+//   subscribe(GET_DETAIL_VIEW, () => appRender('detail', hash));
+//   subscribe(NOT_FOUND, () => appRender('error', hash));
 
 //윈도우 로드시 처리할 내용
-window.onload = async () => {
-  dispatch({
-    type: GET_APP_VIEW,
-    payload: {
-      main: await getMainDataApi(getState().main),
-      best: await getBestDataApi(),
-    },
-  });
-  appRender('home');
-  subscribe(GET_APP_VIEW, () => appRender('main'));
-  subscribe(GET_DETAIL_VIEW, () => appRender('detail'));
-  subscribe(NOT_FOUND, () => appRender('error'));
-  subscribe(GET_LOADING, () => appRender('loading'));
+
+window.onload = () => {
+  const hash = window.location.hash;
+  historyRouterPush(hash);
 };
+
+window.addEventListener('popstate', () => {
+  const hash = window.location.hash;
+  historyRouterPush(hash);
+});
 
 //히스토리 함수
 const historyRouterPush = async pathName => {
   window.history.pushState({}, pathName, window.location.origin + pathName);
   const path = pathName.replace('/', '').replace('#', '');
   if (!path) {
-    appRender('main');
-    return;
+    let mainData = JSON.parse(localStorage.getItem('main'));
+    dispatch({
+      type: GET_APP_VIEW,
+      payload: {
+        main: Object.keys(mainData).length ? mainData : await getMainDataApi(),
+        best: await getBestDataApi(),
+      },
+    });
+    appRender('home', path);
   } else {
-    const data = await getDataApi(path, 'sub');
+    let subData = JSON.parse(localStorage.getItem(path)) || [];
     if (path !== 'bookmark') {
-      dispatch(
-        {
-          type: GET_SUB_VIEW,
-          payload: { path, data: data },
-        },
-        path,
-      );
+      !subData.length && (await getDataApi(path, 'sub'));
     }
+    let { count } = getState().sub;
+    dispatch(
+      {
+        type: GET_SUB_VIEW,
+        payload: { path, data: subData.splice(count * 12, 12) || [], count: 0 },
+      },
+      path,
+    );
     appRender('sub', path);
   }
 };
