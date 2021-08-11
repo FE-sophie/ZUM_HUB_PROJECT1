bfc30e6f8108a9796cb8e5d47706cf54f0bfec54
FE-sophie <saeyoung19@gmail.com>
2021-08-11 12:27:39 +0900

    feat: 크롤링 일부 구현

--- checkDetail.js
+++ checkDetail.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import cheerio from 'cheerio';
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 const getHTML = async url => {
   try {
@@ -9,24 +9,22 @@ const getHTML = async url => {
   }
 };
 
-getHTML()
-  .then(html => {
-    let divList = [];
-    const $ = cheerio.load(html.data);
-    console.log($);
-    const $bodyList = $.querySelector('div#container .contents').children('div');
+const check = url => {
+  getHTML(url).then(({ data }) => {
+    const $ = cheerio.load(data, { decodeEntities: true });
+    const title = $('h2.main_title')[0].children[0].data;
+    const category = $('a#btn_category')[0].children[0].data;
+    const media = $('a#btn_media')[0].children[0].data;
+    const contentList = $('div.article_body')[0].children;
+    // console.log(contentList);
+    let htmlData = {
+      title,
+      category,
+      media,
+      contentList,
+    };
+    return htmlData;
+  });
+};
 
-    $bodyList.each((i, elem) => {
-      divList[i] = {
-        title: $bodyList.querySelector('h2.main_title').text(),
-        url: $bodyList.querySelector('a#btn_media').attr('href'),
-        category: $bodyList.querySelector('a#btn_category').text(),
-        body: $bodyList.querySelector('article_wrap'),
-        url: $bodyList.querySelector('a#btn_media').attr('href'),
-      };
-    });
-    return divList;
-  })
-  .then(res => res);
-
-export default checkData;
+export default check;
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
-// import { getHTML } from 'checkData.js';
-// const cors = require('cors');
 const app = express();
+// import check from 'checkDetail.js';
+// const cors = require('cors');
+
 app.set('port', process.env.PORT || 3000);
 app.set('front', process.env.FRONT || path.join(__dirname, './FrontEnd'));
+
 app.use('/', express.static(path.join(app.get('front'), '/')));
 
 let { food, life, rank, culture, trip, bookmark, best } = require('./data.js');
@@ -20,11 +22,19 @@ const data = {
 app.use(express.json());
 // app.use(cors());
 
+//디테일페이지 데이터 송신
+app.get('/api/detail/:url', (req, res) => {
+  const url = req.params.url;
+  check(url).then(data => res.send(data));
+});
+
+//랭킹 데이터 송신
 app.get('/api/best', (req, res) => {
   const best = data['best'];
   res.send(best);
 });
 
+//북마크 추가 요청
 app.post('/api/bookmark', (req, res) => {
   const { route, id } = req.body;
   if (bookmark.find(el => +id === el.idx)) {
@@ -37,7 +47,7 @@ app.post('/api/bookmark', (req, res) => {
   res.send(bookmark);
 });
 
-//서브페이지 라우팅
+//서브페이지 데이터 송신
 app.get('/api/content/:category', (req, res) => {
   const category = req.params.category;
   res.send(data[category]);
@@ -48,6 +58,8 @@ app.get('/:page', (req, res) => {
   const page = req.params.page;
   res.send(data[page]);
 });
+
+//메인페이지 라우팅
 app.get('/', (req, res) => {
   res.sendFile(path.join(app.get('front'), '/public/index.html'));
 });
