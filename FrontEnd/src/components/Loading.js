a669296ffdbf812b6d8fd0da53590b03d5e26701
FE-sophie <saeyoung19@gmail.com>
2021-08-18 17:32:40 +0900

    fix: loading 이미지 경로 변경

--- FrontEnd/src/components/Loading.js
+++ FrontEnd/src/components/Loading.js
@@ -1,6 +1,6 @@
 const Loading = () => {
   return `<div class="loading">
-            <img src="../../public/loading.gif" alt="로딩중 이미지" />
+            <img src="/public/loading.gif" alt="로딩중 이미지" />
             <span class="loadingText">로딩중.....</span>
           </div>`;
 };
