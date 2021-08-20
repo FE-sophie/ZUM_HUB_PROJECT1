49064dc5399b15b2a45a7e76e27ada8692500705
FE-sophie <saeyoung19@gmail.com>
2021-08-21 02:54:05 +0900

    config:webpack/babel config 파일 생성 및 플러그인 설치

---
+++ webpack.config.js
@@ -0,0 +1,4 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
